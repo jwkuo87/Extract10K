b59c1671878bcb0691f55f88481b692ace0ee5f1
jwkuo87 <jwkuo87@hotmail.com>
2014-05-02 17:02:38 +0200

    Update readability.pl

--- readability.pl
+++ readability.pl
@@ -8,18 +8,20 @@ use Lingua::EN::Fathom;
 
 #All variables
 my $os="WIN";                           #Declare operating system for correct directory handling: WIN for Windows and OSX for Macintosh
-my $folder;                             #Base folder for the 10K filings
-my $subfolder="10K";                   #Subfolder where 10K filings are placed (Optional)
-my $target="Readability";               #Name of destination folder
+my $folder;                             #Base directory for the 10K filings
+my $subfolder="2012";                   #Subdirectory where 10K filings are placed
+my $target="Readability";               #Name of target directory
 my $slash;                              #Declare slash (dependent on operating system)
 my $file;                               #Filename
-my @allfiles;                           #All files in source directory, put into an array
+my @allfiles;                           #All files in directory, put into an array
+my $allfiles;                           #Total files in directory
 my $p;                                  #Variable for progress-bar
 my $c;                                  #Variable for progress-bar
 my $text = new Lingua::EN::Fathom;      #Input file contents
 my $text_string = "";                   #Input file contents in strings
 my $output;                             #Output file with readability statistics
 my $log;                                #Log file (also used to determine point to continue progress)
+my $logfile="$subfolder".".log";        #Filename of log file
 my @filesinlog;                         #Files that have been processed according to log file
 my $accumulate = 1;
 
@@ -33,7 +35,7 @@ elsif($os eq "WIN")
 {
 #Set folders for Windows. Put raw 10K filings in folder\subfolder
 $slash="\\";
-$folder="C:\\10K";
+$folder="C:\\10K\\10K_Full\\";
 }
 
 elsif($os eq "OSX")
@@ -57,29 +59,24 @@ $|=1;
 $p=new Time::Progress;
 $p->attr(min => 0, max => scalar @allfiles);
 $c=0;
+$allfiles=scalar @allfiles;
 
 #Check if output file is present
-if (-e "$folder$slash$subfolder$slash$target$slash"."output.txt"){}
+if (-e "$folder$slash$subfolder$slash$target$slash$subfolder"."_output.txt"){}
 else
 {
-open $output, ">", "$folder$slash$subfolder$slash$target$slash"."output.txt" or die $!;
-print $output "Filename;Gunning Fog Score;Flesch-Kincaid;Sentences;Words;Unique Words;Syllables;Character Count";
+open $output, ">", "$folder$slash$subfolder$slash$target$slash$subfolder"."_output.txt" or die $!;
+print $output "$folder$subfolder\n\nFilename;Gunning Fog Score;Flesch-Kincaid;Sentences;Words;Unique Words;Syllables;Character Count\n";
 close $output;
 }
 
 #Check if log file is present
-if (-e "$folder$slash$subfolder$slash$target$slash"."log.txt")
+if (-e "$folder$slash$subfolder$slash$target$slash$logfile")
 {
-open (FH, "<", "$folder$slash$subfolder$slash$target$slash"."log.txt") or die $!;
+open (FH, "<", "$folder$slash$subfolder$slash$target$slash$logfile") or die $!;
 @filesinlog = <FH>;
 chomp(@filesinlog);
 close FH or die $!;
-}
-else
-{
-open $log, ">>", "$folder$slash$subfolder$slash$target$slash"."log.txt" or die $!;
-print $log "Filename:\n";
-close $log;
 }
 
 foreach my $file(@allfiles)
@@ -90,43 +87,32 @@ else
     $text->analyse_file("$folder$slash$subfolder$slash$file");
     $text->analyse_block($text_string,$accumulate);
 
-    my $fog                     = $text->fog;
-    #my $flesch                  = $text->flesch;
-    my $kincaid                 = $text->kincaid;
+    my $fog                     = sprintf "%.1f", $text->fog;
+    my $kincaid                 = sprintf "%.1f", $text->kincaid;
     my $num_sentences           = $text->num_sentences;
     my $num_words               = $text->num_words;
-    my $words                   = $text->unique_words;
-    #my $syllables               = $text->num_syllables;
+    my %words                   = $text->unique_words;
+    my $unique_words            = keys %words;
+    my $num_syllables           = $text->{num_syllables};
     my $num_chars               = $text->num_chars;
-    my $num_text_lines          = $text->num_text_lines;
-    my $syllables_per_word      = $text->syllables_per_word;
-    my $words_per_sentence      = $text->words_per_sentence;
-    my $percent_complex_words   = $text->percent_complex_words;
-    
     my $readability             = 
                 "$file"
         . ";" . "$fog"
         . ";" . "$kincaid"
         . ";" . "$num_sentences"
         . ";" . "$num_words"
-        . ";" . ""
-        . ";" . ""
+        . ";" . "$unique_words"
+        . ";" . "$num_syllables"
         . ";" . "$num_chars"
         . "\n";
         
-
-        # foreach $word ( sort keys %words )
-        # {
-        # print("$words{$word} :$word\n");
-        # }
-
     foreach my $search ($text)
         {
-        open $output, ">>", "$folder$slash$subfolder$slash$target$slash"."output.txt" or die $!;
+        open $output, ">>", "$folder$slash$subfolder$slash$target$slash$subfolder"."_output.txt" or die $!;
         print $output $readability;
         close $output;
     
-        open $log, ">>", "$folder$slash$subfolder$slash$target$slash"."log.txt" or die $!;
+        open $log, ">>", "$folder$slash$subfolder$slash$target$slash$logfile" or die $!;
         print $log "$file\n";
         close $log;
         }
@@ -134,7 +120,8 @@ else
 
 #Update progress
 $c++;
-print $p->report("%45b %p\r", $c);
+print $p->report("$c/$allfiles files processed: %L %20b %p\r", $c);
 }
 
-print $p->report("$c files processed: %L (%l sec) \n", $c);
+#Print job duration
+print $p->report("$c/$allfiles files processed: %L \n", $c);
